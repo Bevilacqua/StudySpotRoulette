0e7818ab292ce415b7b62f11c28367a3c16f06ae
Jacob Bevilacqua <jacobbev@gmail.com>
2017-03-11 14:00:39 -0800

    Add dependencies for Heroku

--- main.go
+++ main.go
@@ -11,5 +11,9 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
   http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+  fmt.Println("listening...")
+  err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+  if err != nil {
+    panic(err)
+  }
 }
